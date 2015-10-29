35864d08987a2c07829b5a3a4db4894d5b51bd81
Anthony Watts <anthony.watts@capp.co>
2015-10-29 14:15:17 +0000

    I am not even sure where these commits go!

--- Bricks/Models/User.cs
+++ Bricks/Models/User.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bricks.Models
 {
@@ -13,12 +14,24 @@ namespace Bricks.Models
     public class User
     {
         public int ID { get; set; }
+                
+        [Display(Name = "Registration ID")]
         public string AspNetUsersID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "User Type")]
         public UserType InitialUserType { get; set; }
 
         public virtual ICollection<Property> Properties { get; set; }
         public virtual ICollection<Bid> BidsMade { get; set; }
     }
 }
+
